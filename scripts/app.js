55d2cfb7599b48d6d4a92d3d19d6100f3b61b2d2
Esat IBIS <esat.taha.ibis@gmail.com>
2016-02-04 20:01:36 -0500

    Redirect with JavaScript is added to the page. IIFE tested  and it successfuly works.

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,15 @@
  * File name app.js
  * Desc contains custom JavaScript code for Esat's Bio
  */
+
+(function(){
+
+	//alert("Tryout 1 2 3");
+
+})();
+
+function redirectButton() {
+	alert("You succesfully sent your massage!");
+	window.location.href = "index.html";
+}
+
