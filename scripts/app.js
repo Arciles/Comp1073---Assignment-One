c7bafc22e508c3aef02e895c49a8c68ec79ff5c1
Esat IBIS <esat.taha.ibis@gmail.com>
2016-02-11 19:43:18 -0500

    Most of the things ready just Project short Explanations and Bio needs to be written.

--- scripts/app.js
+++ scripts/app.js
@@ -14,12 +14,19 @@
 	var myHeading = 'My Biography!';
 	var myCurrentBioOne = 'my Bio that coming from JavaScript';
 	var myCurrentBioTwo = 'my Bio that coming from JavaScript';
+	var projectOneTitle = 'Transformers Animated Series Unoffical Website';
 	var projectOneText = 'Project one text comming from Javascript saddsadsadsadsadsadsadsa';
+	var projectTwoTitle = 'Artifial Intellegence Movie Unofficial Website';
 	var projectTwoText = 'Project two text coming from JavaScript dsadsadasdsadsadsadsadsadsa';
+	var projectThreeTitle = 'Information Architecture Personas';
 	var projectThreeText = 'Project three text coming from JavaScript dsadsadsadsadsadsadsadsad';
 
 	document.getElementById("paragraphOne").innerHTML = myCurrentBioOne;
 	document.getElementById("paragraphTwo").innerHTML = myCurrentBioTwo;
+
+	document.getElementById("projectOneTitle").innerHTML = projectOneTitle;
+	document.getElementById("projectTwoTitle").innerHTML = projectTwoTitle;
+	document.getElementById("projectThreeTitle").innerHTML = projectThreeTitle;
 
 	/*document.getElementById("projectOneText").innerHTML = projectOneText;
 	document.getElementById("projectTwoText").innerHTML = projectTwoText;
@@ -29,6 +36,7 @@
 	document.getElementById("btnSend").addEventListener("click", function(){
 		alert("You succesfully sent your massage!");
 		console.log('success');
+		//this.className += ' scroll-top page-scroll';
 		window.location.href = "#page-top";
 	});
 
